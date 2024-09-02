a1a90c3bfd1379ac8879adff184b772aa9497181
Mahya Asadi <mahyaasadi378@gmail.com>
2024-09-02 21:00:25 +0330

    fixed alias path in tsconfig

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import App from "@/App";
+import "@/index.css";
 import "primeicons/primeicons.css";
 import "primereact/resources/primereact.min.css";
 import "primereact/resources/themes/saga-blue/theme.css";
